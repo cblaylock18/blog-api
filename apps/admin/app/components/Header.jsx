868779eb0f3d69093c0f3cd7944bef68ae9a9c62
Christopher Blaylock <160085222+cblaylock18@users.noreply.github.com>
2025-04-21 19:19:35 -0400

    feat: update site title

--- apps/admin/app/components/Header.jsx
+++ apps/admin/app/components/Header.jsx
@@ -1,7 +1,7 @@
 export function Header() {
     return (
         <header className="text-4xl p-6 pb-0">
-            <h1>Blog API - Public Viewer</h1>
+            <h1>Blog API - Author View</h1>
         </header>
     );
 }
